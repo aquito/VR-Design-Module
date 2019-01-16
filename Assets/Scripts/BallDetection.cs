cdbf388e7385434e38fcfa849366251368fd80f8
Aki <aki@gamefutures.com>
2019-01-16 15:44:42 +0000

    first scripts added

--- Assets/Scripts/BallDetection.cs
+++ Assets/Scripts/BallDetection.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    
+    GameManager gameManager;
+    private void Start() {
+        
 
+    }
     // look for collisions
 
     private void OnCollisionEnter(Collision other) {
         
-        // if(other.)
+        // if other is ball, send the game object this script is attached to, to the gamemanager and it will process it according to object type (floor/prop)
     }
 
-    // if collision is with ball, get its starting position and return it there
-    // play negative feedback sound
     
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public List<GameObject> trackObjects;
+    private List<bool> objectCleared;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // monitor for collisions from balldetection script
+        // if ball has hit floor, get ball starting position and return it there with negative sound
+        // if ball has hit track object (and is not grabbed at that moment by user), flag it as true in the trackObject list    
+    }
+
+
+}
