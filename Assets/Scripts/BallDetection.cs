5805b3745ce0a9f4a3274a3c119a89451af75665
Aki <aki@gamefutures.com>
2019-01-16 15:14:28 +0000

    first set of primitives in place

---
+++ Assets/Scripts/BallDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+
+    // look for collisions
+
+    private void OnCollisionEnter(Collision other) {
+        
+        // if(other.)
+    }
+
+    // if collision is with ball, get its starting position and return it there
+    // play negative feedback sound
+    
+}
---
+++ Assets/Scripts/BallPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPosition : MonoBehaviour
+{
+    public Transform ballStartPosition;
+    void Start()
+    {
+        ballStartPosition = gameObject.transform;
+    }
+
+}
