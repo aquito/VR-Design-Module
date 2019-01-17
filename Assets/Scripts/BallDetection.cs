8fea8cff10802bd5c1da39291b8a902c67ecf8d6
Aki <aki@gamefutures.com>
2019-01-17 17:06:44 +0000

    work on scripts

--- Assets/Scripts/BallDetection.cs
+++ Assets/Scripts/BallDetection.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BallDetection : MonoBehaviour
 {
-    GameManager gameManager;
-    private void Start() {
-        
-
-    }
+   public GameManager gameManager;
+    
     // look for collisions
 
     private void OnCollisionEnter(Collision other) {
         
+        if(other.gameObject.tag == "Ball")
+        {
+            gameManager.ProcessCollision(other.gameObject, this.gameObject);
+        }
         // if other is ball, send the game object this script is attached to, to the gamemanager and it will process it according to object type (floor/prop)
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
     
     public List<GameObject> trackObjects;
-    private List<bool> objectCleared;
+    private Dictionary<GameObject, bool> objectsCleared;
 
     void Start()
     {
-        
+
+        // get the GameObject for objectsCleared dict and set them to false
+
     }
 
     // Update is called once per frame
@@ -21,5 +23,45 @@ public class GameManager : MonoBehaviour
         // if ball has hit track object (and is not grabbed at that moment by user), flag it as true in the trackObject list    
     }
 
+    public void ProcessCollision(GameObject ball, GameObject other)
+    {
 
+        if(trackObjects != null)
+        {
+            for (int i =0; i < trackObjects.Count ; i++)
+            {
+                if(other == trackObjects[i])
+                {
+                   CheckObjectOff(other); 
+                    // check if all objects set on trackObjects are among objectsCleared
+                }
+            }
+            
+
+        } else
+        {
+            Debug.Log("You need to add objects to the track objects list in the inspector!");
+        }
+       
+        Debug.Log(ball.name + " hit " + other);
+    }
+
+    void CheckObjectOff(GameObject obj) // mark object as true on the list
+    {   
+
+         for (int i =0; i < trackObjects.Count ; i++)
+            {
+                if(objectsCleared[obj] == false)
+                {
+                   objectsCleared[obj] = true;
+
+                    // check if object 
+                }
+            }   
+    }
+
+    void CheckIfAllObjects()
+    {
+        // check if there are falses left in the dictionary
+    }
 }
