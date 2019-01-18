0eba312f7d3d820a993b44940b2b6cbbe0286a93
Aki <aki@gamefutures.com>
2019-01-18 14:18:20 +0000

    work on scripts
    audio manager broken

--- Assets/Scripts/BallDetection.cs
+++ Assets/Scripts/BallDetection.cs
@@ -8,11 +8,22 @@ public class BallDetection : MonoBehaviour
     
     // look for collisions
 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {      
+         // FindObjectOfType<GameManager>() as GameManager;
+         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     private void OnCollisionEnter(Collision other) {
         
         if(other.gameObject.tag == "Ball")
         {
             gameManager.ProcessCollision(other.gameObject, this.gameObject);
+
         }
         // if other is ball, send the game object this script is attached to, to the gamemanager and it will process it according to object type (floor/prop)
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     
-    public List<GameObject> trackObjects;
-    private Dictionary<GameObject, bool> objectsCleared;
+    public GameObject[] trackObjects; // array for track prefabs placed in scene
+
+    private Dictionary<GameObject, bool> objectsCleared; // dictionary where each track object is flagged false until true (=once ball has touched them) 
+
+    bool isTrackCompleted; // flag for checking completion
+
+    public GameObject audioSelector;
 
     void Start()
     {
 
-        // get the GameObject for objectsCleared dict and set them to false
+      isTrackCompleted = false;
 
+      trackObjects = GameObject.FindGameObjectsWithTag("TrackObject");
+
+        foreach(GameObject trackObject in trackObjects)
+        {
+            Debug.Log(trackObject.name + " found.");
+            objectsCleared[trackObject] = false;
+        }   
+
+       // audioSelector = gameObject.FindGameObject("AudioSelector").GetComponent<AudioSelector>();
+        // audioSelector = audioSelector.FindObjectOfType<AudioSelector>() as AudioSelector;
     }
-
-    // Update is called once per frame
+    
+    
     void Update()
     {
         // monitor for collisions from balldetection script
@@ -26,22 +41,32 @@ public class GameManager : MonoBehaviour
     public void ProcessCollision(GameObject ball, GameObject other)
     {
 
-        if(trackObjects != null)
+        if(other.tag == "EndPoint" && isTrackCompleted)
         {
-            for (int i =0; i < trackObjects.Count ; i++)
-            {
-                if(other == trackObjects[i])
-                {
-                   CheckObjectOff(other); 
-                    // check if all objects set on trackObjects are among objectsCleared
-                }
-            }
-            
-
-        } else
-        {
-            Debug.Log("You need to add objects to the track objects list in the inspector!");
+            TrackCompleted();
+            Debug.Log("Track cleared!");
         }
+        else
+        {
+            
+        }
+
+             if(trackObjects != null)
+            {
+                for (int i =0; i < trackObjects.Length ; i++)
+                {
+                    if(other == trackObjects[i])
+                    {
+                    CheckObjectOff(other); 
+                    // check if all objects set on trackObjects are among objectsCleared
+                    }       
+                }
+            
+            } else
+            {
+                Debug.Log("You need to add objects to the track objects list in the inspector!");
+            }
+        
        
         Debug.Log(ball.name + " hit " + other);
     }
@@ -49,19 +74,39 @@ public class GameManager : MonoBehaviour
     void CheckObjectOff(GameObject obj) // mark object as true on the list
     {   
 
-         for (int i =0; i < trackObjects.Count ; i++)
+         for (int i =0; i < trackObjects.Length ; i++)
             {
                 if(objectsCleared[obj] == false)
                 {
                    objectsCleared[obj] = true;
+                   Debug.Log(obj + "cleared by ball");
 
                     // check if object 
                 }
             }   
     }
 
-    void CheckIfAllObjects()
+    void CheckIfAllObjects()  // check if there are falses left in the dictionary
     {
-        // check if there are falses left in the dictionary
+        foreach(KeyValuePair<GameObject, bool> entry in objectsCleared)
+        {
+            if(entry.Value)
+            {
+                break;
+            } else
+            {
+                isTrackCompleted = true;
+                Debug.Log("All track objectc cleared with the ball");
+            }
+        }
+       
     }
+
+    void TrackCompleted()
+    {
+        // this is called by 
+        audioSelector.PlaySFX("win");
+        
+    }
+
 }
