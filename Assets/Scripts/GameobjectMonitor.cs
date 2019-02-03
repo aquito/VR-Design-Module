f6ba5a643b02638852c7104f7a7b1fd4dac8d8e7
Aki <aki@gamefutures.com>
2019-02-03 15:39:08 +0000

    more project cleaning

---
+++ Assets/Scripts/GameobjectMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameobjectMonitor : MonoBehaviour
+{
+    //  get object sequence object
+    //  check for collisions between object sequence objects and 'objectFetchDestination' ie the object where fetched objects need to be brought
+    //  call MarkObjectAsFound when a legit collision happens, call only once & set isKinematic as true on the sequence object  
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
