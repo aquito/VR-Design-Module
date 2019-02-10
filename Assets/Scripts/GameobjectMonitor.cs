d6e6706c365703414fe8a22a62530ad8095dbcd7
Aki <aki@gamefutures.com>
2019-02-10 20:48:56 +0000

    Escape room demo for class

--- Assets/Scripts/GameobjectMonitor.cs
+++ Assets/Scripts/GameobjectMonitor.cs
@@ -6,18 +6,25 @@ public class GameobjectMonitor : MonoBehaviour
 {
     
      GameObjectSequence objectSequence;
+
+     AudioSource audioSource;
     
     
     void Start()
     {
         objectSequence = GameObject.Find("GameObjectSequence").GetComponent<GameObjectSequence>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision other) {
 
+        audioSource.Play();
+
         Debug.Log(other.gameObject.name + " hit " + this.name);
         
         objectSequence.MarkObjectAsFound(other.gameObject);
+
+        
         
     }
 }
